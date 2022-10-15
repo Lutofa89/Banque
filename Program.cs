161db36f42605d4048db877032ef7a1a9013ccaa
Fabrice Kerremans <[200~kerremans.1904.student@ifosupwavre.[200~^[[200~kerremans.1904.student@ifosupwavre>
2022-10-15 12:26:47 +0200

    programme banque

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿//Créer une classe "Personne" implémentant:
+//  les propriétés publiques
+//  string Name
+//  string LastName
+//  DateTime BirthDate
+
+namespace Banque
+{
+
+
+    public class Personne
+    {
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+    }
+    
+    public class CurrentAccount
+    {
+        public string Number { get; set; }  
+        public double Balance { get; set; }
+
+        public double CreditLine { get; set; }
+
+        public Personne Owner { get; set; }
+
+        void Withdrawat(double amount)
+        {
+            Balance = Balance - amount;
+        }
+
+        public class Bank
+        {
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
